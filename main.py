b33e1f3a9c518ab28ffe798fc6acba679a5e7022
Bastian <badmaxyt@gmail.com>
2025-08-03 05:14:06 +0200

    docs: :memo: update openapi description

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from fastapi import FastAPI
 from src.api import endpoints
 
 app = FastAPI(
-    title="API de détection d'objets Yolo",
+    title="API de détection d'objets",
     description="""
-### Service de prédiction d'objets propulsé par YOLOv8
+### Service de prédiction d'objets propulsé par YOLOv11
 
-Cette API expose un modèle de détection d'objets personnalisé, entraîné avec YOLOv8.
+Cette API expose un modèle de détection d'objets personnalisé, entraîné avec YOLOv11.
 
 Elle permet de soumettre des images et de recevoir en retour les prédictions
 sous forme de boîtes englobantes, de classes et de scores de confiance.
 
-Le code source du projet est disponible sur GitHub :
+Le code source du projet d'entraînement du model est disponible sur GitHub :
 [https://github.com/jvondermarck/ai-object-detection/](https://github.com/jvondermarck/ai-object-detection/)
 """,
 )
